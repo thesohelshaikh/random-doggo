beeea25ae0fbeabfd226484429e8d37a1a88bf57
Sohel Shaikh <sohelshaikh97@gmail.com>
2025-02-22 13:01:06 +0530

    Add image loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,9 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
+    implementation("io.coil-kt.coil3:coil-network-ktor3:3.1.0")
+    implementation("com.jakewharton.timber:timber:5.0.1")
 
 
 }
--- app/src/main/java/com/example/randomdoggo/generate/GenerateDogsScreen.kt
+++ app/src/main/java/com/example/randomdoggo/generate/GenerateDogsScreen.kt
@@ -3,12 +3,28 @@ package com.example.randomdoggo.generate
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.ColorImage
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import com.example.randomdoggo.R
 import com.example.randomdoggo.ui.theme.RandomdoggoTheme
 
 @Composable
@@ -19,16 +35,37 @@ fun GenerateDogsScreen(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
     ) {
         Text("Generate Dogs!")
+
+        AsyncImage(
+            modifier = Modifier
+                .padding(16.dp)
+                .size(100.dp),
+            placeholder = painterResource(R.drawable.ic_launcher_background),
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_4x3.jpg")
+                .crossfade(true)
+                .build(),
+            contentDescription = "Image of a dog",
+        )
+
         Button(onClick = {}) {
             Text("Generate!")
         }
     }
 }
 
-@Preview
+@OptIn(ExperimentalCoilApi::class)
+@Preview(showBackground = true)
 @Composable
 private fun PreviewGenerateDogsScreen() {
-    RandomdoggoTheme {
-        GenerateDogsScreen()
+    val previewHandler = AsyncImagePreviewHandler {
+        ColorImage(Color.Red.toArgb())
     }
+
+    CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
+        RandomdoggoTheme {
+            GenerateDogsScreen()
+        }
+    }
+
 }
